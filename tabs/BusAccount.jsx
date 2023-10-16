a3e6669afb34ab78a534185ea9c66246dc9f8e04
IsuruX98 <isurusanka98@gmail.com>
2023-10-16 21:26:23 +0530

    backend connected

--- tabs/BusAccount.jsx
+++ tabs/BusAccount.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text, StyleSheet,TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import jwt_decode from "jwt-decode";
+import axios from "../apis/axios";
 
 const BusAccount = ({navigation}) => {
+
+    const [loggedInUser,setLoggedInUser] = useState({})
+    const [loggedInUserEmail,setLoggedInUserEmail] = useState(null)
+    let users = {}
+
+    useEffect(() => {
+        // Check the token in AsyncStorage when the component mounts
+        const checkToken = async () => {
+            try {
+                const token = await AsyncStorage.getItem('JWT');
+
+                if (token) {
+                    const decodedToken = jwt_decode(token);
+                    const userEmail = String(decodedToken.sub); // Convert to string
+                    setLoggedInUserEmail(userEmail);
+
+                    if (userEmail){
+                        const response = await axios.get("bus");
+                        const data = response.data.body;
+
+                        const usersWithEmails = data
+                            .find(entry => entry.user && userEmail === entry.user.email)
+                        users = usersWithEmails
+                        setLoggedInUser(usersWithEmails);
+                    }
+
+                } else {
+                    console.log('No token found in AsyncStorage');
+                }
+            } catch (error) {
+                console.error('Error reading token from AsyncStorage:', error);
+            }
+        };
+
+        checkToken(); // Call the function when the component mounts
+    }, []);
 
     const handleLogout = async () => {
         try {
@@ -19,21 +57,21 @@ const BusAccount = ({navigation}) => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.heading}>Bus Account</Text>
+            <Text style={styles.heading}>Profile</Text>
             <View style={styles.profileContainer}>
                 <Ionicons name="person-circle-outline" size={150} color="#555" />
             </View>
             <View style={styles.detailsContainer}>
-                <Text style={styles.label}>Bus ID:</Text>
-                <Text style={styles.value}>12345</Text>
                 <Text style={styles.label}>Bus Number:</Text>
-                <Text style={styles.value}>AB 123</Text>
-                <Text style={styles.label}>Owner Name:</Text>
-                <Text style={styles.value}>John Doe</Text>
+                <Text style={styles.value}>{loggedInUser.busNo}</Text>
+                <Text style={styles.label}>Inspector Name:</Text>
+                <Text style={styles.value}>{loggedInUser.user?loggedInUser.user.name:''}</Text>
+                <Text style={styles.label}>Mobile:</Text>
+                <Text style={styles.value}>{loggedInUserEmail}</Text>
                 <Text style={styles.label}>Bus Route ID:</Text>
-                <Text style={styles.value}>6789</Text>
+                <Text style={styles.value}>{loggedInUser.route?loggedInUser.route.routeName:''}</Text>
                 <Text style={styles.label}>Income:</Text>
-                <Text style={styles.value}>Rs.6789.00</Text>
+                <Text style={styles.value}>Rs.{loggedInUser.income}</Text>
                 <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
                     <Text style={styles.buttonTxt}>Logout</Text>
                 </TouchableOpacity>
